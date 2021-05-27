1a845219dc3fa64c70e72a10151abe84ea155f59
ofacklam <oliver.facklam@gmail.com>
2021-05-27 13:07:50 +0200

    allocation implementation with segregated free lists & bitmap

--- l3/vm/c/src/memory.c
+++ l3/vm/c/src/memory.c
@@ -13,6 +13,11 @@ static value_t* bitmap = NULL;
 static value_t* heap_start = NULL;
 
 #define HEADER_SIZE 1
+#define MIN_BLOCK_SIZE 1
+#define BITS_PER_WORD (8 * sizeof(value_t))
+#define FREE_LIST_SIZE 32
+
+static value_t free_list[FREE_LIST_SIZE]; // points to the block start virtual address
 
 // Virtual <-> physical address translation
 
@@ -41,7 +46,94 @@ static value_t header_unpack_size(value_t header) {
 
 // Bitmap management
 
+static uint8_t bitmap_is_set(const value_t* block) {
+    assert(block >= heap_start);
+    value_t mem_offset = (value_t) (block - heap_start);
+    value_t bitmap_idx = mem_offset / BITS_PER_WORD;
+    uint8_t bitmap_offset = mem_offset % BITS_PER_WORD;
+    return (bitmap[bitmap_idx] >> bitmap_offset) & 0x01;
+}
 
+static void bitmap_set(const value_t* block) {
+    assert(block >= heap_start);
+    value_t mem_offset = (value_t) (block - heap_start);
+    value_t bitmap_idx = mem_offset / BITS_PER_WORD;
+    uint8_t bitmap_offset = mem_offset % BITS_PER_WORD;
+    bitmap[bitmap_idx] |= (1u << bitmap_offset);
+}
+
+static void bitmap_clear(const value_t* block) {
+    assert(block >= heap_start);
+    value_t mem_offset = (value_t) (block - heap_start);
+    value_t bitmap_idx = mem_offset / BITS_PER_WORD;
+    uint8_t bitmap_offset = mem_offset % BITS_PER_WORD;
+    bitmap[bitmap_idx] &= ~(1u << bitmap_offset);
+}
+
+// Free list management
+
+static void free_list_insert(value_t* block) {
+    value_t size = memory_get_block_size(block);
+    if(size >= FREE_LIST_SIZE)
+        size = FREE_LIST_SIZE - 1;
+    value_t old_head = free_list[size];
+    *block = old_head;
+    free_list[size] = addr_p_to_v(block);
+}
+
+static void free_list_remove(const value_t* block, value_t* prev_block) {
+    value_t next_block = *block;
+    *prev_block = next_block;
+}
+
+static value_t* free_list_next(const value_t* block) {
+    return addr_v_to_p(*block);
+}
+
+static value_t* free_list_find(value_t size) {
+    value_t start_index = size < FREE_LIST_SIZE ? size : FREE_LIST_SIZE - 1;
+
+    for(value_t k = start_index; k < FREE_LIST_SIZE; k++) {
+        value_t* prev_block = &free_list[k];
+        value_t* block;
+        while((block = free_list_next(prev_block)) != memory_start) {
+            if(memory_get_block_size(block) >= size) {
+                free_list_remove(block, prev_block);
+                return block;
+            }
+            prev_block = block;
+        }
+    }
+
+    return memory_start;
+}
+
+// Allocation / de-allocation
+
+static value_t* try_allocate(tag_t tag, value_t size) {
+    // find appropriate block
+    value_t* block = free_list_find(size);
+    if(block == memory_start) // no block available
+        return memory_start;
+
+    // split the block
+    value_t additional_space = memory_get_block_size(block) - size;
+    if(additional_space > 0) {
+        value_t* split_block = block + size + HEADER_SIZE;
+        split_block[-HEADER_SIZE] = header_pack(tag_None, additional_space - HEADER_SIZE);
+        if(additional_space >= HEADER_SIZE + MIN_BLOCK_SIZE) { // enough space for a real block -> add to free list
+            free_list_insert(split_block);
+        }
+    }
+
+    // update bitmap
+    bitmap_set(block);
+
+    // set header
+    block[-HEADER_SIZE] = header_pack(tag, size);
+
+    return block;
+}
 
 // Interface implementation
 
@@ -76,25 +168,25 @@ void memory_set_heap_start(void* hs) {
   if(free_words <= 0)
       fail("No memory left for heap");
 
-  size_t heap_words = (size_t) (32 * free_words / 33);
-  size_t bitmap_words = (size_t) ceil( heap_words / 32.);
+  value_t heap_words = BITS_PER_WORD * (value_t) free_words / (BITS_PER_WORD + 1);
+  value_t bitmap_words = (value_t) ceil((double) heap_words / BITS_PER_WORD);
 
   bitmap = hs;
   heap_start = bitmap + bitmap_words;
+
+  value_t* whole_memory_block = heap_start + HEADER_SIZE;
+  whole_memory_block[-HEADER_SIZE] = header_pack(tag_None, heap_words - HEADER_SIZE);
+  free_list_insert(whole_memory_block);
 }
 
 value_t* memory_allocate(tag_t tag, value_t size, roots_t* roots) {
-  assert(free_boundary != NULL);
+  assert(heap_start != NULL);
 
-  const value_t total_size = size + HEADER_SIZE;
-  if (free_boundary + total_size > memory_end)
+  value_t* block = try_allocate(tag, size);
+  if (block == memory_start)
     fail("no memory left (block of size %u requested)", size);
 
-  *free_boundary = header_pack(tag, size);
-  value_t* res = free_boundary + HEADER_SIZE;
-  free_boundary += total_size;
-
-  return res;
+  return block;
 }
 
 void memory_allocate_lb_ob(value_t l_size, value_t o_size, roots_t* roots) {
@@ -103,9 +195,9 @@ void memory_allocate_lb_ob(value_t l_size, value_t o_size, roots_t* roots) {
 }
 
 value_t memory_get_block_size(value_t* block) {
-  return header_unpack_size(block[-1]);
+  return header_unpack_size(block[-HEADER_SIZE]);
 }
 
 tag_t memory_get_block_tag(value_t* block) {
-  return header_unpack_tag(block[-1]);
+  return header_unpack_tag(block[-HEADER_SIZE]);
 }
